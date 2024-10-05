e54d3371bf526292be773181c24609a6d310ad64
chTink <champgn@icloud.com>
2024-10-04 23:58:03 -0700

    running a server

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("servidor is running!");
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor running on 3000')
+})
